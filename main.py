d1c6b612b4617254d5d6d01fd7378347f62b6c7e
= <mukherjee4004@gmail.com>
2023-08-28 22:54:44 +0530

    2

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import uvicorn
-from dotenv import load_dotenv
-load_dotenv()
-import os
-port = int(os.getenv("PORT", default=8000))
-if __name__ == "__main__":
-  uvicorn.run("server.api:app", host="0.0.0.0", port=port, reload=True)
+from fastapi import FastAPI
+
+app = FastAPI()
+app.get("/")
+
